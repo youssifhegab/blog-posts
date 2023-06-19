b0fcb9d442015b0bd9b31494bd49fe69be53917a
youssifhegab <youssif.hegab@zyda.com>
2023-06-20 00:09:28 +0300

    Add read more and read less

--- src/features/posts/components/Post.jsx
+++ src/features/posts/components/Post.jsx
@@ -1,19 +1,9 @@
 import { useState } from "react";
 import { ShimmerTitle } from "react-shimmer-effects";
-import { motion } from "framer-motion";
 
 const Post = ({ post, isLoading }) => {
   const [isExpanded, setIsExpanded] = useState(false);
   const { title, body } = post || {};
-
-  const navLinkVariants = {
-    hidden: {
-      height: "fit-content",
-    },
-    visible: {
-      height: "max-content",
-    },
-  };
 
   if (isLoading) {
     return (
@@ -26,12 +16,7 @@ const Post = ({ post, isLoading }) => {
   return (
     <div className='w-full bg-white rounded-lg py-4 px-5 shadow-md '>
       <p className='mb-4 font-semibold text-lg truncate'>{title}</p>
-      <motion.div
-        variants={navLinkVariants}
-        initial='hidden'
-        animate={isExpanded ? "visible" : "hidden"}
-        className='flex text-gray-500'
-      >
+      <div className='flex text-gray-500'>
         <p className='inline-block transition ease-in-out delay-150 resize-none'>
           {`${isExpanded ? body : body.substring(0, 100)}...`}{" "}
           <p
@@ -41,7 +26,7 @@ const Post = ({ post, isLoading }) => {
             {isExpanded ? "Read less" : "Read more"}
           </p>
         </p>
-      </motion.div>
+      </div>
     </div>
   );
 };
